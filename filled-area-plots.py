12ab98acaef7dbe900315a945ce96bc44321023a
santhosh-graceson <119423698+santhosh-graceson@users.noreply.github.com>
2023-06-13 11:56:19 +0530

    filled-area-plots.py

--- filled-area-plots.py
+++ filled-area-plots.py
@@ -2,7 +2,7 @@ from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 
 app = Dash(__name__)
-
+server=app.server
 app.layout = html.Div([
     html.H4("Country's key performance analytics"),
     html.P("Select data on y-axis:"),
